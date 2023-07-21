f01a479b5b3bc664ebaccedb90324f5326099377
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-07-21 13:42:12 +0700

    use RTK Query to fetch API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 
 import { decrement, increment, reset } from "@/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useGetUsersQuery } from "@/redux/services/userApi";
+import Image from "next/image";
 
 export default function Home() {
   const count = useAppSelector((state) => state.counterReducer.value);
   const dispatch = useAppDispatch();
+
+  const { isLoading, isFetching, data, error } = useGetUsersQuery(null);
 
   return (
     <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
@@ -20,6 +24,36 @@ export default function Home() {
         </button>
         <button onClick={() => dispatch(reset())}>reset</button>
       </div>
+
+      {error ? (
+        <p>Oh no, ther was an error</p>
+      ) : isLoading || isFetching ? (
+        <p>Loading...</p>
+      ) : data ? (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gap: 20,
+          }}
+        >
+          {data.map((user) => (
+            <div
+              key={user.id}
+              style={{ border: "1px solid #ccc", textAlign: "center" }}
+            >
+              <Image
+                src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+                alt={user.name}
+                style={{ height: 180, width: 180 }}
+                width={180}
+                height={180}
+              />
+              <h3>{user.name}</h3>
+            </div>
+          ))}
+        </div>
+      ) : null}
     </main>
   );
 }
