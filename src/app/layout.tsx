d804fac5c493ba64d9ee2ba66595e812aef42f4a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-07-21 10:40:09 +0700

    litle change for styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <body>{children}</body>
       </Providers>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <div>Homepage</div>
-  )
+    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
+      <div style={{ marginBottom: "4rem", textAlign: "center" }}>
+        <h4 style={{ marginBottom: 16 }}>0</h4>
+        <button>increment</button>
+        <button style={{ marginInline: 16 }}>decrement</button>
+        <button>reset</button>
+      </div>
+    </main>
+  );
 }
