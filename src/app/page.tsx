9f91757b3102636ce2612c1d93863fc02f8fe29a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-07-21 10:43:24 +0700

    Use Redux State and Actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+
+import { decrement, increment, reset } from "@/redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+
 export default function Home() {
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+
   return (
     <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
       <div style={{ marginBottom: "4rem", textAlign: "center" }}>
-        <h4 style={{ marginBottom: 16 }}>0</h4>
-        <button>increment</button>
-        <button style={{ marginInline: 16 }}>decrement</button>
-        <button>reset</button>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          style={{ marginInline: 16 }}
+          onClick={() => dispatch(decrement())}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
       </div>
     </main>
   );
